97a6ddb1d481d6ab713d4024c1b4c8b55250ee2b
Tony Anter <anthony_anter@bmc.com>
2023-02-23 12:25:19 -0700

    Tony change

--- COB/CWXTSUBC.cbl
+++ COB/CWXTSUBC.cbl
@@ -6,7 +6,8 @@
       *                                                                *
       *  THIS CALLED PROGRAM IS THE COBOL DEMO PROGRAM USED FOR        *
       *  XPEDITER/TSO TRAINING SESSIONS.                               *
-      *                                                                *
+      *           
+      * Tony was here 2/23/2023                                        *
       ******************************************************************
        ENVIRONMENT DIVISION.
        DATA DIVISION.
